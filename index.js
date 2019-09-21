6d1e81ca84e903ec9a167b6038b39e4696af0c4d
EC2 Default User <ec2-user@ip-172-31-5-253.us-east-2.compute.internal>
2019-09-21 17:47:01 +0000

    some more code

--- index.js
+++ index.js
@@ -10,10 +10,23 @@ const syncAndSeed = async()=> {
     await conn.sync({ force: true });
     const products = [
         { name: 'foo' },
+        { name: 'foo2' },
         { name: 'bar' },
         { name: 'bazz' }
 ];
-    
+ 
+ 
+const [ foo1, foo2, bar, bazz ] = await mapAndCreate(products, Product);
+
+return {
+    products: {
+        foo1,
+        foo2,
+        bar,
+        bazz
+    }
+};
+
 await mapAndCreate(products, Product);
 
 };
--- product.js
+++ product.js
@@ -2,5 +2,13 @@ const conn = require('./conn');
 const { UUID, UUID4, STRING } = conn.Sequelize;
 
 const Product = conn.define('product', {
+    id: {
+        primaryKey: true,
+        type: UUID,
+        defaultValue: UUIDV4
+    },
+    name: STRING
     
 });
+
+module.exports = Product;
