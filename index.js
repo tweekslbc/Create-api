2df2aced33db870cb3ab42fce99c13ede229a762
EC2 Default User <ec2-user@ip-172-31-5-253.us-east-2.compute.internal>
2019-09-21 17:34:36 +0000

    setting up index.js

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-console.log("way too much info");
+const conn = require('./conn');
+const Product = require('./Product');
+
+const mapAndCreate = ()=> {
+    
+}
+
+const syncAndSeed = async()=> {
+    const products = [
+        { name: 'foo' },
+        { name: 'bar' },
+        { name: 'bazz' }
+];
+    
+await mapAndCreate(products, Product);
+
+};
+
+module.exports = {
+    syncAndSeed
+};
